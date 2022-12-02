3b51a3ed63284a7cc57e8b55709193990a4dedd3
stesiam <52320488+stesiam@users.noreply.github.com>
2022-12-02 07:22:12 +0200

    feat: unique input for every party

--- ui.R
+++ ui.R
@@ -1,4 +1,13 @@
 htmlTemplate("template.html",
-             button = actionButton("action", "Action"),
-             slider = sliderInput("x", "", 1, 51 , 25.5,step = 0.1)
+             button = actionButton("action", "Calculate !"),
+             
+             slider_nd = sliderInput("nd", "", 25, 50 , 39.9, step = 0.1),
+             slider_syriza = sliderInput("syriza", "", 20, 45 , 31,5, step = 0.1),
+             slider_pasok = sliderInput("pasok", "", 5, 15 , 8.1, step = 0.1),
+             slider_kke = sliderInput("kke", "", 1, 10, 5.3, step = 0.1),
+             slider_ellisi = sliderInput("ellisi", "",  1, 10 , 3.7, step = 0.1),
+             slider_mera25 = sliderInput("mera25", "",  1, 10 , 3.4, step = 0.1),
+             slider_other1 = sliderInput("other1", "",  1, 10, 1, step = 0.1),
+             slider_other2 = sliderInput("other2", "",  1, 10, 1, step = 0.1),
+             slider_other3 = sliderInput("other3", "",  1, 10, 1, step = 0.1),
 )
