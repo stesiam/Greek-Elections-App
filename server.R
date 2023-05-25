640298e68e195f37f3b0f3816744b7dc822af33a
stesiam <52320488+stesiam@users.noreply.github.com>
2023-05-25 21:26:39 +0300

    Add more parties according to recent election results

--- server.R
+++ server.R
@@ -3,7 +3,7 @@ server <- function(input, output) {
   inparl = reactive({
     x = c(input$nd, input$syriza, input$pasok,
     input$kke, input$ellisi, input$mera25,
-    input$other1, input$other2, input$other3)
+    input$niki, input$plefsi, input$other1)
     inparl = 0
     toallocate = 100
     for (i in 1:length(x)){
@@ -19,10 +19,11 @@ server <- function(input, output) {
   output$pasok_seats <- renderText({  nobonus(input$pasok, inparl()[[1]])  })
   output$kke_seats <- renderText({  nobonus(input$kke, inparl()[[1]])  })
   output$ellisi_seats <- renderText({  nobonus(input$ellisi, inparl()[[1]])  })
+  output$niki_seats <- renderText({ nobonus(input$niki, inparl()[[1]])  })
+  output$plefsi_seats <- renderText({ nobonus(input$plefsi, inparl()[[1]]) })
   output$mera25_seats <- renderText({  nobonus(input$mera25, inparl()[[1]])  })
-  output$other1_seats <- renderText({ nobonus(input$other1, inparl()[[1]])  })
+  output$other1_seats <- renderText({ nobonus(input$other1, inparl()[[1]]) })
   output$other2_seats <- renderText({ nobonus(input$other2, inparl()[[1]]) })
-  output$other3_seats <- renderText({ nobonus(input$other3, inparl()[[1]]) })
   
   output$toallocate_server <- renderText({ inparl()[[2]] })
   output$oop_server <- renderText({ 100 - inparl()[[1]] })
